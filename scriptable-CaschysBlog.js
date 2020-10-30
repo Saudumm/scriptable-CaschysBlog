7fd8891396102577e17d674f0f59252a88dc92a3
Christoph Kehl <saudumm1@googlemail.com>
2020-10-30 15:43:24 +0100

    v1.4 - bug fixes and custom background images

--- scriptable-CaschysBlog.js
+++ scriptable-CaschysBlog.js
@@ -1,31 +1,44 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: deep-blue; icon-glyph: city;
-// v1.1 coded by Saudumm
+// v1.4 coded by Saudumm
 
-// WIDGET CONFIG
-var WIDGET_SIZE = 'small'; // small, medium, large // lässt sich durch Widget Parameter überschreiben
-const SITE_URL = 'https://www.stadt-bremerhaven.de';
-const SITE_NAME = 'Caschys Blog';
-const POST_IMAGES = true; // auf false setzen, wenn Bilder-URL unklar oder Bilder unerwünscht sind
-// Note: Die Kombination POST_IMAGES = true + WIDGET_SIZE = small ignoriert die BACKGROUND_GRADIENT_COLOR Werte bei kleinen Widgets
+// WIDGET PARAMETER: small|https://www.stadt-bremerhaven.de|Caschys Blog|background.jpg
+// parameter order has to be: widget size, site url, site name, background image
+// parameters have to be separated by |
+// You can omit parameters, for example background image: small|https://www.stadt-bremerhaven.de|Caschys Blog
+// or you can just set "small", "medium" or "large" as parameter
+// parameters that are not set will be set by the standard widget config
+
+// STANDARD WIDGET CONFIG -> can be overwritten by parameters
+var WIDGET_SIZE = 'small'; // small, medium, large // can be overwritten with widget parameters
+var SITE_URL = 'https://www.stadt-bremerhaven.de'; // address of the website to fetch posts from
+var SITE_NAME = 'Caschys Blog'; // name to display in the widget
+var BACKGROUND_IMAGE_NAME = "none"; // CASE SENSITIVE!!! filename of the custom background image, set to none if you don't want a custom image
+var BACKGROUND_IMAGE_GRADIENT = true; // gradient over the background image
+// custom background images have to be in the Scriptable iCloud Files directory (same as the script js file)
 
 // COLOR CONFIG
-var BACKGROUND_GRADIENT = false; // Widget Hintergrund; true = Farbverlauf, false = einfarbig
-var BACKGROUND_COLOR = new Color("#1c1c1e"); // Wird verwendet wenn BACKGROUND_GRADIENT = false
-var BACKGROUND_GRADIENT_COLOR_TOP = new Color("#48484a"); // Farbverlauf Farbe oben
-var BACKGROUND_GRADIENT_COLOR_BTM = new Color("#2c2c2e"); // Farbverlauf Farbe unten
-const FONT_COLOR_SITENAME = Color.white(); // Schriftfarbe Seitenname
-const FONT_COLOR_HEADLINE = Color.white(); // Schriftfarbe Schlagzeile
-const FONT_COLOR_POST_DATE = Color.lightGray(); // Schriftfarbe Datum/Uhrzeit
+const POST_IMAGES = true; // set to false if you don't want images
+// Note: combining POST_IMAGES = true + WIDGET_SIZE = small will ignore BACKGROUND_GRADIENT_COLOR values in small widgets
+var BACKGROUND_GRADIENT = false; // widget background; true = gradient, false = single background color
+var BACKGROUND_COLOR = new Color("#1c1c1e"); // color value if BACKGROUND_GRADIENT = false
+var BACKGROUND_GRADIENT_COLOR_TOP = new Color("#48484a"); // gradient color top
+var BACKGROUND_GRADIENT_COLOR_BTM = new Color("#2c2c2e"); // gradient color bottom
+const FONT_COLOR_SITENAME = Color.white(); // font color website name (SITE_NAME)
+const FONT_COLOR_HEADLINE = Color.white(); // font color for headlines
+const FONT_COLOR_POST_DATE = Color.gray(); // font color for date/time
 
 // DO NOT CHANGE
-// JSON URL für Posts (Wordpress Standard)
+// JSON URL for Posts (WordPress Standard)
 if (args.widgetParameter) {
-    WIDGET_SIZE = args.widgetParameter;
-} else {
-    WIDGET_SIZE = WIDGET_SIZE;
+    let widgetParameter = args.widgetParameter.split("|");
+    if (widgetParameter.length >= 1) { WIDGET_SIZE = widgetParameter[0]; }
+    if (widgetParameter.length >= 2) { SITE_URL = widgetParameter[1]; }
+    if (widgetParameter.length >= 3) { SITE_NAME = widgetParameter[2]; }
+    if (widgetParameter.length >= 4) { BACKGROUND_IMAGE_NAME = widgetParameter[3]; }
 }
+
 var NUMBER_OF_POSTS = 2;
 switch (WIDGET_SIZE) {
     case 'small':
@@ -38,7 +51,8 @@ switch (WIDGET_SIZE) {
         NUMBER_OF_POSTS = 5;
         break;
 }
-const JSON_API_URL = SITE_URL+"/wp-json/wp/v2/posts/?filter[category_name]=country&per_page="+NUMBER_OF_POSTS;
+
+const JSON_API_URL = SITE_URL+"/wp-json/wp/v2/posts";
 
 let widget = await createWidget();
 
@@ -71,16 +85,16 @@ async function createWidget(items) {
     
     if (data) {
         if (NUMBER_OF_POSTS == 1) {
-            // Hintergrundbild laden
-            if (POST_IMAGES == true) {
-                list.backgroundImage = await getImage(data.post1Thumbnail);
-
-                // Gradient über Hintergrundbild, damit Text lesbar wird
+            // load widget background image (if POST_IMAGES = true or BACKGROUND_IMAGE_NAME is set)
+            if (POST_IMAGES == true && BACKGROUND_IMAGE_NAME == "none") {
+                list.backgroundImage = await getImage(data.post1BG);
+                
+                // draw gradient over background image for better readability
                 BACKGROUND_GRADIENT = true;
-                BACKGROUND_GRADIENT_COLOR_TOP = new Color('#000000', 0.4);
+                BACKGROUND_GRADIENT_COLOR_TOP = new Color('#000000', 0.25);
                 BACKGROUND_GRADIENT_COLOR_BTM = new Color('#000000', 1);
-   
-                // Für bessere Lesbarkeit ein kleiner Schatten um den Seitennamen
+       
+                // small shadow outline on SITE_NAME for better readability
                 siteName.shadowRadius = 1;
                 siteName.shadowColor = Color.black();
             }
@@ -102,7 +116,7 @@ async function createWidget(items) {
             list.url = data.post1URL;
             
         } else if (NUMBER_OF_POSTS >= 2) {
-            // Reihe für Post 1
+            // row for post 1
             const stackRow1 = list.addStack();
             stackRow1.layoutHorizontally();
             stackRow1.url = data.post1URL;
@@ -131,7 +145,7 @@ async function createWidget(items) {
             }
             list.addSpacer();
 
-            // Reihe für Post 2
+            // row for post 2
             const stackRow2 = list.addStack();
             stackRow2.layoutHorizontally();
             stackRow2.url = data.post2URL;
@@ -162,7 +176,7 @@ async function createWidget(items) {
             if (NUMBER_OF_POSTS == 5) {
                 list.addSpacer();
 
-                // Reihe für Post 3
+                // row for post 3
                 const stackRow3 = list.addStack();
                 stackRow3.layoutHorizontally();
                 stackRow3.url = data.post3URL;
@@ -192,7 +206,7 @@ async function createWidget(items) {
                 
                 list.addSpacer();
                 
-                // Reihe für Post 4
+                // row for post 4
                 const stackRow4 = list.addStack();
                 stackRow4.layoutHorizontally();
                 stackRow4.url = data.post4URL;
@@ -222,7 +236,7 @@ async function createWidget(items) {
 
                 list.addSpacer();
                 
-                // Reihe für Post 5
+                // row for post 5
                 const stackRow5 = list.addStack();
                 stackRow5.layoutHorizontally();
                 stackRow5.url = data.post5URL;
@@ -252,12 +266,31 @@ async function createWidget(items) {
             }
         }
     } else {
-        const error_msg = list.addText("Keine Daten gefunden");
+        const error_msg = list.addText("No data found");
         error_msg.font = Font.systemFont(10);
     }
     
-    // Hintergrund des Widget (Gradient oder einzelne Farbe)
-    if (BACKGROUND_GRADIENT == true) {
+    // widget background (single color or gradient)
+    if (BACKGROUND_IMAGE_NAME != "none") {
+        let imageLocalURL = await getLocalImage(BACKGROUND_IMAGE_NAME);
+        if (imageLocalURL != "not found") {
+            let customBackgroundImage = await Image.fromFile(imageLocalURL);
+            list.backgroundImage = customBackgroundImage;
+            
+            if (BACKGROUND_IMAGE_GRADIENT == true) {
+                // draw gradient over background image for better readability
+                const gradient = new LinearGradient();
+                gradient.locations = [0, 1];
+                gradient.colors = [
+                   new Color('#000000', 0.25),
+                   new Color('#000000', 1)
+                ];
+                list.backgroundGradient = gradient;
+            }
+        } else {
+            list.backgroundColor = BACKGROUND_COLOR;
+        }
+    } else if (BACKGROUND_GRADIENT == true) {
         const gradient = new LinearGradient();
         gradient.locations = [0, 1];
         gradient.colors = [
@@ -285,7 +318,10 @@ async function getData() {
         if (NUMBER_OF_POSTS >= 1) {
             post1Title = loadedJSON[0].title.rendered;
             post1Title = formatHeadline(post1Title);
-            if (POST_IMAGES == true) { post1ThumbnailURL = await getMediaURL(loadedJSON[0].featured_media); }
+            if (POST_IMAGES == true) {
+                post1ThumbnailURL = await getMediaURL(loadedJSON[0].featured_media);
+                post1ThumbnailURL = encodeURI(post1ThumbnailURL);
+            }
             post1URL = loadedJSON[0].guid.rendered;
             post1DateTime = loadedJSON[0].date;
             
@@ -293,32 +329,45 @@ async function getData() {
                 let featuredMediaJSONURL = SITE_URL+"/wp-json/wp/v2/media/"+loadedJSON[0].featured_media;
                 let loadedMediaJSON = await new Request(featuredMediaJSONURL).loadJSON();
                 post1BG = loadedMediaJSON.media_details.sizes.medium_large.source_url;
+                post1ThumbnailURL = encodeURI(post1BG);
             }
             
             if (NUMBER_OF_POSTS >= 2) {
                 post2Title = loadedJSON[1].title.rendered;
                 post2Title = formatHeadline(post2Title);
                 post2DateTime = loadedJSON[1].date;
-                if (POST_IMAGES == true) { post2ThumbnailURL = await getMediaURL(loadedJSON[1].featured_media); }
+                if (POST_IMAGES == true) {
+                    post2ThumbnailURL = await getMediaURL(loadedJSON[1].featured_media);
+                    post2ThumbnailURL = encodeURI(post2ThumbnailURL);
+                }
                 post2URL = loadedJSON[1].guid.rendered;
                 
                 if (NUMBER_OF_POSTS == 5) {
                     post3Title = loadedJSON[2].title.rendered;
                     post3Title = formatHeadline(post3Title);
                     post3DateTime = loadedJSON[2].date;
-                    if (POST_IMAGES == true) { post3ThumbnailURL = await getMediaURL(loadedJSON[2].featured_media); }
+                    if (POST_IMAGES == true) {
+                        post3ThumbnailURL = await getMediaURL(loadedJSON[2].featured_media);
+                        post3ThumbnailURL = encodeURI(post3ThumbnailURL);
+                    }
                     post3URL = loadedJSON[2].guid.rendered;
                     
                     post4Title = loadedJSON[3].title.rendered;
                     post4Title = formatHeadline(post4Title);
                     post4DateTime = loadedJSON[3].date;
-                    if (POST_IMAGES == true) { post4ThumbnailURL = await getMediaURL(loadedJSON[3].featured_media); }
+                    if (POST_IMAGES == true) {
+                        post4ThumbnailURL = await getMediaURL(loadedJSON[3].featured_media);
+                        post4ThumbnailURL = encodeURI(post4ThumbnailURL);
+                    }
                     post4URL = loadedJSON[3].guid.rendered;
                     
                     post5Title = loadedJSON[4].title.rendered;
                     post5Title = formatHeadline(post5Title);
                     post5DateTime = loadedJSON[4].date;
-                    if (POST_IMAGES == true) { post5ThumbnailURL = await getMediaURL(loadedJSON[4].featured_media); }
+                    if (POST_IMAGES == true) {
+                        post5ThumbnailURL = await getMediaURL(loadedJSON[4].featured_media);
+                        post5ThumbnailURL = encodeURI(post5ThumbnailURL);
+                    }
                     post5URL = loadedJSON[4].guid.rendered;
                 }
             }
@@ -345,6 +394,7 @@ async function getData() {
             post3URL: post3URL,
             post4URL: post4URL,
             post5URL: post5URL,
+            post1BG: post1BG
         };
 
         return result;
@@ -390,7 +440,7 @@ function formatHeadline(strHeadline) {
 function convertDateString(strDate) {
     let date_conv = new Date(strDate);
     let dateTimeLocal = date_conv.toLocaleString([], {year: 'numeric', month: 'numeric', day: 'numeric', hour: '2-digit', minute: '2-digit'})
-    return dateTimeLocal
+	return dateTimeLocal
 }
 
 async function getImage(url) {
@@ -403,4 +453,16 @@ async function getMediaURL(featuredMedia) {
     let loadedMediaJSON = await new Request(featuredMediaJSONURL).loadJSON();
     let mediaURL = loadedMediaJSON.media_details.sizes.thumbnail.source_url;
     return mediaURL;
+}
+
+async function getLocalImage(imageName) {
+    var fm = FileManager.iCloud();
+    let dir = fm.documentsDirectory()
+    let backgroundImagePath = fm.joinPath(dir, imageName)
+
+    if (fm.fileExists(backgroundImagePath) == true) {
+        return backgroundImagePath;
+    } else {
+        return "not found";
+    }
 }
