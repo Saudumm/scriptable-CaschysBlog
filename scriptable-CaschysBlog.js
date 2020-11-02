c7df3aa75095004589015a78676712333bde5157
Christoph Kehl <saudumm1@googlemail.com>
2020-11-02 21:58:43 +0100

    v1.6.1 - datetime locale fix

--- scriptable-CaschysBlog.js
+++ scriptable-CaschysBlog.js
@@ -337,7 +337,7 @@ function formatPostTitle(strHeadline) {
 // result: a nicely formatted date and time
 function convertJSONDateString(strDate) {
   let date_conv = new Date(strDate);
-  let dateTimeLocal = date_conv.toLocaleString(["en-US"], {year: "numeric", month: "2-digit", day: "2-digit", hour: "2-digit", minute: "2-digit"})
+  let dateTimeLocal = date_conv.toLocaleString([], {year: "numeric", month: "2-digit", day: "2-digit", hour: "2-digit", minute: "2-digit"})
   return dateTimeLocal
 }
 
